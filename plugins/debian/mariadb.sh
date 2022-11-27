3890a069c624abb540d9124698e278d2b8b7e0c3
nvnhan <nguyenvannhan0810@gmail.com>
2022-11-27 15:04:21 +0700

    Add Beekeeper Studio

--- plugins/debian/mariadb.sh
+++ plugins/debian/mariadb.sh
@@ -1,11 +1,15 @@
-#! /bin/bash 
+#! /bin/bash
 
 sudo apt-get update && sudo apt-get upgrade
 
-sudo apt-get install mariadb-server mariadb-client -y
+if [ ! "$(mariadb --version)" ]; then
+	sudo apt-get install mariadb-server mariadb-client -y
 
-sudo systemctl restart mariadb 
+	sudo systemctl restart mariadb
 
-sudo systemctl enable mariadb
+	sudo systemctl enable mariadb
 
-sudo mysql_secure_installation
+	sudo mysql_secure_installation
+fi
+
+sudo snap install beekeeper-studio
--- plugins/osx/mariadb.sh
+++ plugins/osx/mariadb.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-brew install mariadb
+if [ ! "$(mariadb --version)" ]; then
+	brew install mariadb
 
-brew services restart mariadb
+	brew services restart mariadb
 
-sudo mysql_secure_installation
+	sudo mysql_secure_installation
+fi
+
+brew install --cask beekeeper-studio
