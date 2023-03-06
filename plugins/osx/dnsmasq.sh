d4e0b5f77a2ccf81a019fd1587a3ab3e87582d37
Van Nhan <nguyenvannhan0810@gmail.com>
2023-03-06 20:31:49 +0700

    Update command for dnsmasq'

--- plugins/osx/dnsmasq.sh
+++ plugins/osx/dnsmasq.sh
@@ -7,4 +7,4 @@ echo 'address=/.test/127.0.0.1' > /usr/local/etc/dnsmasq.conf
 sudo mkdir -v /etc/resolver
 sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
 
-sudo launchctl start homebrew.mxcl.dnsmasq
+sudo brew services start dnsmasq
