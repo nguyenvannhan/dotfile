28344c791bfdef7a616dddebcac88a6abb5c1204
nhannv-zigexn <nhannv@zigexn.vn>
2023-04-24 13:46:55 +0700

    Update keymap for diagnostic goto_next

--- configs/nvim/lua/nhannv/lsp/lsp-saga.lua
+++ configs/nvim/lua/nhannv/lsp/lsp-saga.lua
@@ -12,7 +12,7 @@ saga.setup({
 })
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<S-C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<leader>,', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_diagnostic<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
